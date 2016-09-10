93cf2963fa11023f4cae61da9d884de068937b34
Steve Barnett <steve@naga.co.za>
2016-09-10 15:22:38 +0200

    Make CSS a bit non-block-y

--- functions.php
+++ functions.php
@@ -1,9 +1,26 @@
 <?php
 function load_theme_styles_and_scripts() {
-  wp_enqueue_style( 'theme-styles', get_template_directory_uri() . '/css/style.min.css', null, '1.01', 'all' );
+  if ( isset($_COOKIE['cachedCSS']) && $_COOKIE['cachedCSS'] === 'true' ) {
+    wp_enqueue_style( 'theme-styles', get_template_directory_uri() . '/css/style.min.css', null, '1.01', 'all' );
+  }
   	wp_enqueue_script( 'theme-scripts', get_template_directory_uri() . '/js/html5shiv.min.js', null, '1.0', true );
 }
 add_action('wp_enqueue_scripts', 'load_theme_styles_and_scripts');
+
+function load_css_in_header() {
+  if ( !isset($_COOKIE['cachedCSS']) || $_COOKIE['cachedCSS'] !== 'true' ) :
+  ?>
+    <style type="text/css">
+      <?php echo file_get_contents( get_template_directory_uri() . '/css/style.min.css' ); ?>
+    </style>
+    <script>
+        var expires = new Date(+new Date + (7 * 24 * 60 * 60 * 1000)).toUTCString();
+        document.cookie = 'cachedCSS=true; expires=' + expires;
+    </script>
+  <?php
+  endif;
+}
+add_action('wp_head', 'load_css_in_header', 30);
 
 // register_nav_menus( array(
 //   'primary' => __( 'Primary Menu', 'pcm' ),
